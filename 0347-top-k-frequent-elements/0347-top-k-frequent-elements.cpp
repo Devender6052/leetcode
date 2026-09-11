393f42c48d34496e23034fbb67a6d53f829750f6
Devender <2327uiet.devender6052@kuk.ac.in>
2026-09-11 17:34:10 +0530

    Time: 0 ms (100%), Space: 17.9 MB (75.8%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        vector<int> ans;
+        unordered_map<int,int> mp;
+        for(auto x: nums){
+            mp[x]++;
+        }
+        vector<pair<int,int>> v(mp.begin(),mp.end());
+        sort(v.begin(),v.end(),[](auto &a,auto &b){
+            return a.second>b.second;
+        });
+        for(int i=0;i<k;i++){
+            ans.push_back(v[i].first);
+        }
+        return ans;
+    }
+};
